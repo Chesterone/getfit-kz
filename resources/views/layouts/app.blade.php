9a7c596695f564f86bdcd8a9ced2a3412398da85
vagrant <vagrant@homestead>
2018-12-13 16:46:22 +0000

    Green header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,37 +20,35 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    <header>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel bg-success">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    GetFit.kz
+                    <svg class="text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
+                    <strong class="text-white">GetFit.kz</strong>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">Вход</a>
+                                <a class="nav-link text-white" href="{{ route('login') }}">Вход</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">Регистрация</a>
+                                    <a class="nav-link text-white" href="{{ route('register') }}">Регистрация</a>
                                 </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
@@ -73,12 +71,20 @@
                 </div>
             </div>
         </nav>
-
-        <main class="py-4">
+    </div>
+    </header>
+    <main class="py-4">
             @include('inc.messages')
             @yield('content')
-        </main>
-    </div>
+    </main>
+    <footer class="text-muted">
+            <div class="container">
+              <p class="float-right">
+                <a href="#">К началу страницы</a>
+              </p>
+              <p>Сайт создан в качестве проектной работы на базе &copy; Bootstrap, с использованием PHP, HTML, MySQL и CSS.</p>
+            </div>
+          </footer>
     <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
     <script>
         CKEDITOR.replace( 'article-ckeditor' );
