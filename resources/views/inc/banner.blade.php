d1d3c87ab2ce24fd6c091075e61bd0b4845a0763
Alibek Tokayev <atokayev@gmail.com>
2019-03-02 13:00:12 -0500

    articles links added

---
+++ resources/views/inc/banner.blade.php
@@ -0,0 +1,10 @@
+<section class="jumbotron text-center">
+      <div class="container">
+        <h1 class="jumbotron-heading">Всё о правильном питании</h1>
+        <p class="lead text-muted">Перед началом работы с сайтом рекомендуем вам ознакомиться с информацией по следующим ссылкам:</p>
+        <p>
+          <a href="{{url('advice')}}" class="btn btn-success my-2">Общие советы по правильному питанию</a>
+          <a href="{{url('unhealthy')}}" class="btn btn-danger my-2">Какие продукты следует исключить из рациона?</a>
+        </p>
+      </div>
+    </section>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -70,19 +70,8 @@
     </header>
 
     <main role="main">
+    @include('inc.banner')
     @include('inc.messages')
-    @section('banner')
-    <section class="jumbotron text-center">
-      <div class="container">
-        <h1 class="jumbotron-heading">Всё о правильном питании</h1>
-        <p class="lead text-muted">Перед началом работы с сайтом рекомендуем вам ознакомиться с информацией по следующим ссылкам:</p>
-        <p>
-          <a href="{{url('advice')}}" class="btn btn-success my-2">Общие советы по правильному питанию</a>
-          <a href="{{url('unhealthy')}}" class="btn btn-danger my-2">Какие продукты следует исключить из рациона?</a>
-        </p>
-      </div>
-    </section>
-    @endsection
     @yield('content')
     </main>
 
@@ -91,7 +80,7 @@
         <p class="float-right">
           <a href="#">К началу страницы</a>
         </p>
-        <p>Сайт создан в качестве проектной работы на базе &copy; Bootstrap, с использованием PHP, HTML и CSS.</p>
+        <p>Сайт создан в качестве проектной работы на базе &copy; Bootstrap, с использованием PHP, Mysql, HTML и CSS.</p>
       </div>
     </footer>
 
